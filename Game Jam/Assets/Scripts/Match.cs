7ce2bc45c283c70c81e58c27a1810d7016fa5cf4
DarthSaffa <rowen.koortzen@gmail.com>
2016-09-15 16:50:13 +1000

    Started the lighting scripts

---
+++ Game Jam/Assets/Scripts/Match.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Match : MonoBehaviour {
+
+	public static float matchLightLevel = 100;
+	public Light matchGlow;
+	int extinguishRate = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		matchGlow.intensity = matchLightLevel / 50;
+
+		matchLightLevel -= extinguishRate * Time.deltaTime;
+
+		if(matchGlow.intensity <= 0.1){
+			matchGlow.intensity = 0.1f;
+		}
+	}
+}
